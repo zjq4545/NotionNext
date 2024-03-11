60aa752f8d9dbe0de4746abc1da171552696cce8
zjq4545 <162955441+zjq4545@users.noreply.github.com>
2024-03-11 16:39:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '艾影晨光', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '影视资源爱好者', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.ai-hd.com/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
