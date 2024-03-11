7a811d9fd8b088d2e16830741472c00c2d473490
zjq4545 <162955441+zjq4545@users.noreply.github.com>
2024-03-11 20:45:00 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   HEO_HERO_TITLE_4: '',
   HEO_HERO_TITLE_5: '欢迎收藏，交流学习',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_LINK: '',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
